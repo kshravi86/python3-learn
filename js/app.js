caf764c8d2f521f3c69f0ce15b0f6a85db29e5b4
kshravi86 <kshravi86@gmail.com>
2018-07-08 11:56:34 +0530

    svveve

--- js/app.js
+++ js/app.js
@@ -18,7 +18,7 @@ var myApp = angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.d
         $routeProvider.when('/view3', {templateUrl: 'partials/asarray.html'});
         $routeProvider.when('/view4', {templateUrl: 'partials/ndarray basics.py.html'});
         $routeProvider.when('/view5', {templateUrl: 'partials/frombuffer.py.html'});
-        $routeProvider.when('/view6', {templateUrl: 'partials/CielandDancing.html'});
+        $routeProvider.when('/view6', {templateUrl: 'partials/data types.py.html'});
         $routeProvider.when('/view7', {templateUrl: 'partials/BusinessTrip.html'});
         $routeProvider.when('/view8', {templateUrl: 'partials/AmusingJoke.html'});
 		$routeProvider.when('/view9',{templateUrl:'partials/ChoosingTeams.html'});
