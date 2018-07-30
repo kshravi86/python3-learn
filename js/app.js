0d3d1f22a0e581007da682b90e1d700f44854ab4
kshravi86 <kshravi86@gmail.com>
2018-07-30 15:43:47 +0530

    vrhhcc

--- js/app.js
+++ js/app.js
@@ -32,7 +32,7 @@ var myApp = angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.d
 	   $routeProvider.when("/view17",{templateUrl:'partials/P10_LCM.py.html'});
 	   $routeProvider.when("/view18",{templateUrl:'partials/P17_EvenOdd.py.html'});
 	   $routeProvider.when("/view19",{templateUrl:'partials/P72_PythonLambda.py.html'});
-	  $routeProvider.when("/view20",{templateUrl:'partials/Football.html'});
+	  $routeProvider.when("/view20",{templateUrl:'partials/P12_DecimalToBinary.py.html'});
 	  $routeProvider.when("/view21",{templateUrl:'partials/Game.html'});
 	  $routeProvider.when("/view22",{templateUrl:'partials/cAPSlOCK.html'});
 	  $routeProvider.when("/view23",{templateUrl:'partials/IQtest.html'});
